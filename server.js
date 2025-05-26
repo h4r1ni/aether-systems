aa485b8548ef3fa72fff409210acd144ff9eb0a4
h4r1ni <hrnvenkatesan@gmail.com>
2025-05-26 15:31:48 +0100

    Update server to use index.html as homepage

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'professional-ui.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Universal checkout session endpoint
